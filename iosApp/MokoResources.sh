5be2137532fea2a4ee22e456df6af8783ab9f432
aaermakov <aaermakov@corp.finam.ru>
2024-02-20 10:04:08 +0300

    Fix.

--- iosApp/MokoResources.sh
+++ iosApp/MokoResources.sh
@@ -1,5 +1,6 @@
-"$SRCROOT/../gradlew" -p "$SRCROOT/../" :myresources:copyFrameworkResourcesToApp \
-    -Pmoko.resources.PLATFORM=$PLATFORM_NAME \
-    -Pmoko.resources.CONFIGURATION=$CONFIGURATION \
-    -Pmoko.resources.BUILT_PRODUCTS_DIR=$BUILT_PRODUCTS_DIR \
-    -Pmoko.resources.CONTENTS_FOLDER_PATH=$CONTENTS_FOLDER_PATH
+"$SRCROOT/../gradlew" -p "$SRCROOT/../" :myresources:copyPodFrameworkResourcesToApp \
+    -Pmoko.resources.BUILT_PRODUCTS_DIR="$BUILT_PRODUCTS_DIR" \
+    -Pmoko.resources.CONTENTS_FOLDER_PATH="$CONTENTS_FOLDER_PATH" \
+    -Pkotlin.native.cocoapods.platform="$PLATFORM_NAME" \
+    -Pkotlin.native.cocoapods.archs="$ARCHS" \
+    -Pkotlin.native.cocoapods.configuration="$CONFIGURATION"
